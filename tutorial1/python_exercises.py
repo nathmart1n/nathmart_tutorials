a8ddf8e5eb2f5741d3221cbfa5b2c6f22baf8580
Nathan Martin <45038512+nathmart1n@users.noreply.github.com>
2020-10-04 15:26:30 -0700

    Completed tutorial 1

--- tutorial1/python_exercises.py
+++ tutorial1/python_exercises.py
@@ -3,35 +3,40 @@ Intro to python exercises shell code
 """
 
 def is_odd(x):
-    """
-    returns True if x is odd and False otherwise
-    """
+    if ((x % 2) == 0):
+        return True
+    else:
+        return False
 
 
 def is_palindrome(word):
-    """
-    returns whether `word` is spelled the same forwards and backwards
-    """
+    return word == word[::-1]
 
 
 def only_odds(numlist):
     """
     returns a list of numbers that are odd from numlist
-
     ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
     """
+    return [num for num in numlist if num % 2 == 0]
 
 
 def count_words(text):
     """
     return a dictionary of {word: count} in the text
-
     words should be split by spaces (and nothing else)
     words should be converted to all lowercase
-
     ex: count_words("How much wood would a woodchuck chuck"
                     " if a woodchuck could chuck wood?")
         ->
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
+    word_dict = {}
+    words = text.split()
+
+    for word in words:
+        if word not in word_dict.keys():
+            word_dict[word] == 1
+        else:
+            word_dict += 1
